7e3ec93d6a14baf1645df3c57d7d3e064e5e340b
Ronan <manvelia@bc.edu>
2020-03-04 13:13:06 -0800

    Completed 7.2

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Ronan Manvelian on 3/4/20.
+//  Copyright © 2020 Ronan Manvelian. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
