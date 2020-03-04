e854609fdc71e3eb3fa794aa590d866ded2e95e6
Ronan <manvelia@bc.edu>
2020-03-04 12:34:18 -0800

    Initial Commit

---
+++ WeatherGift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherGift
+//
+//  Created by Ronan Manvelian on 3/4/20.
+//  Copyright © 2020 Ronan Manvelian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
