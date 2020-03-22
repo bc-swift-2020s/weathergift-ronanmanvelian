6b3b73a5ba7ced468d8fe05bd191d9240e914ec6
Ronan Manvelian <manvelia@bc.edu>
2020-03-22 14:51:56 -0700

    Completed 7.3 and 7.4 With .gitignore

---
+++ WeatherGift/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  WeatherGift
+//
+//  Created by Ronan Manvelian on 3/22/20.
+//  Copyright © 2020 Ronan Manvelian. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyCHQkeu8LuS8iCJVORvDrw5A0dMapyLWLM"
+}
