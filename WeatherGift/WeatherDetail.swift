cae052a017b6d6fd4555b757ee731be417840ee8
Ronan Manvelian <manvelia@bc.edu>
2020-03-23 12:38:49 -0700

    Completed 7.11

---
+++ WeatherGift/WeatherDetail.swift
@@ -0,0 +1,68 @@
+//
+//  WeatherDetail.swift
+//  WeatherGift
+//
+//  Created by Ronan Manvelian on 3/23/20.
+//  Copyright © 2020 Ronan Manvelian. All rights reserved.
+//
+
+import Foundation
+
+class WeatherDetail: WeatherLocation {
+    
+    struct Result: Codable {
+        var timezone: String
+        var currently: Currently
+        var daily: Daily
+    }
+    
+    struct Currently: Codable {
+        var temperature: Double
+    }
+    
+    struct Daily: Codable {
+        var summary: String
+    }
+    
+    var timezone = ""
+    var temperature = 0
+    var summary = ""
+    
+    func getData(completed: @escaping () -> () ) {
+        let coordinates = "\(latitude),\(longitude)"
+        let urlString = "\(APIurls.darkSkyURL)\(APIKeys.darkSkyKey)/\(coordinates)"
+        print("🕸 We are accessing the url \(urlString)")
+        
+        // Create a URL
+        guard let url = URL(string: urlString) else {
+            print("😡 ERROR: Could not create a URL from \(urlString)")
+            completed()
+            return
+        }
+        
+        // Create Session
+        let session = URLSession.shared
+        
+        // Get data with .dataTask method
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("😡 ERROR: \(error.localizedDescription)")
+            }
+            
+            // Note: there are some additional things that could go wrong when using URL session, but we shouldn't experience them, so we'll ignore testing for these things for now...
+            
+            // Deal with the data
+            do {
+                let result = try JSONDecoder().decode(Result.self, from: data!)
+                self.timezone = result.timezone
+                self.temperature = Int(result.currently.temperature.rounded())
+                self.summary = result.daily.summary
+            } catch {
+                print("😡 JSON ERROR: \(error.localizedDescription)")
+            }
+            completed()
+        }
+        
+        task.resume()
+    }
+}
--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -19,37 +19,4 @@ class WeatherLocation: Codable {
         self.longitude = longitude
     }
     
-    func getData() {
-        let coordinates = "\(latitude),\(longitude)"
-        let urlString = "\(APIurls.darkSkyURL)\(APIKeys.darkSkyKey)/\(coordinates)"
-        print("🕸 We are accessing the url \(urlString)")
-        
-        // Create a URL
-        guard let url = URL(string: urlString) else {
-            print("😡 ERROR: Could not create a URL from \(urlString)")
-            return
-        }
-        
-        // Create Session
-        let session = URLSession.shared
-        
-        // Get data with .dataTask method
-        let task = session.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                print("😡 ERROR: \(error.localizedDescription)")
-            }
-            
-            // Note: there are some additional things that could go wrong when using URL session, but we shouldn't experience them, so we'll ignore testing for these things for now...
-            
-            // Deal with the data
-            do {
-                let json = try JSONSerialization.jsonObject(with: data!, options: [])
-                print("😎 \(json)")
-            } catch {
-                print("😡 JSON ERROR: \(error.localizedDescription)")
-            }
-        }
-        
-        task.resume()
-    }
 }
